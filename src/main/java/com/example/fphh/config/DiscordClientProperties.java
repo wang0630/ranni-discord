8dee65c3430102db1b31f282b89377160b1412d6
wang0630 <j2081499@gmail.com>
2023-04-30 12:02:28 +0800

    Add VoiceChannelUpdate event

--- src/main/java/com/example/fphh/config/DiscordClientProperties.java
+++ src/main/java/com/example/fphh/config/DiscordClientProperties.java
@@ -3,10 +3,13 @@ package com.example.fphh.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Data
+@PropertySource("classpath:discord.properties")
 @ConfigurationProperties("discord.props")
 public class DiscordClientProperties {
   private String token;
+  private String generalTextChannelId;
 }
---
+++ src/main/java/com/example/fphh/service/VoiceChannelUpdateService.java
@@ -0,0 +1,54 @@
+package com.example.fphh.service;
+
+import com.example.fphh.config.DiscordClientProperties;
+import discord4j.common.util.Snowflake;
+import discord4j.core.event.domain.VoiceStateUpdateEvent;
+import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.channel.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class VoiceChannelUpdateService implements DiscordService<VoiceStateUpdateEvent> {
+  private final DiscordClientProperties discordClientProperties;
+  @Override
+  public Class<VoiceStateUpdateEvent> getEventType() {
+    return VoiceStateUpdateEvent.class;
+  }
+
+  @Override
+  public Mono<Void> execute(VoiceStateUpdateEvent event) {
+    if (!event.isJoinEvent()) {
+      return Mono.empty();
+    }
+
+    VoiceState vs = event.getCurrent();
+
+    return vs.getGuild()
+        .flatMap(guild -> {
+          return Mono.zip(
+              guild.getChannelById(Snowflake.of(discordClientProperties.getGeneralTextChannelId())),
+              vs.getUser()
+          );
+        })
+        .flatMap(tuple -> {
+          // Channel map here: https://javadoc.io/doc/com.discord4j/discord4j-core/3.0.9/discord4j/core/object/entity/Channel.Type.html
+          Channel.Type type = tuple.getT1().getType();
+          if (type == Channel.Type.GUILD_TEXT) {
+            TextChannel mc = (TextChannel) tuple.getT1();
+            return mc.createMessage("Welcome back, Tarnished " + tuple.getT2().getUsername());
+          } else {
+            return Mono.empty();
+          }
+        })
+        .then();
+  }
+
+  private Mono<VoiceChannel> getChannel(VoiceState vs) {
+    return vs.getChannel();
+  }
+}
